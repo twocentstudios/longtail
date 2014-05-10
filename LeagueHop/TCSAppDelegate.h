61d0912b042098b7cb85f7fbbef0a666b09e8dd8
Chris Trott <chris@twocentstudios.com>
2014-05-10 01:24:22 -0400

    Initial Commit

---
+++ LeagueHop/TCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCSAppDelegate.h
+//  LeagueHop
+//
+//  Created by Chris Trott on 5/10/14.
+//  Copyright (c) 2014 TwoCentStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LeagueHop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LeagueHop
+//
+//  Created by Chris Trott on 5/10/14.
+//  Copyright (c) 2014 TwoCentStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCSAppDelegate class]));
+    }
+}

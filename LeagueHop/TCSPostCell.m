fac03a313f9e993e65dbbface1c2672d8dd9d528
Chris Trott <chris@twocentstudios.com>
2014-05-25 20:03:27 -0400

    Post cell bindings

--- LeagueHop/TCSPostCell.m
+++ LeagueHop/TCSPostCell.m
@@ -7,6 +7,19 @@
 
 #import "TCSPostViewModel.h"
 
+@interface TCSPostCell ()
+
+@property (nonatomic) UILabel *nameLabel;
+@property (nonatomic) UILabel *postSummaryLabel;
+@property (nonatomic) UILabel *dateLabel;
+@property (nonatomic) UILabel *messageLabel;
+@property (nonatomic) UIImageView *linkImageView;
+@property (nonatomic) UILabel *linkNameLabel;
+@property (nonatomic) UILabel *likesLabel;
+@property (nonatomic) UILabel *commentsLabel;
+
+@end
+
 #pragma mark -
 
 @implementation TCSPostCell
@@ -16,8 +29,16 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
     if (self != nil) {
-        RAC(self.textLabel, text) = RACObserve(self, viewModel.message);
-        RAC(self.detailTextLabel, text) = RACObserve(self, viewModel.year);
+        RAC(self.nameLabel, attributedText) = RACObserve(self, viewModel.userName);
+        RAC(self.postSummaryLabel, attributedText) = RACObserve(self, viewModel.postSummary);
+        RAC(self.dateLabel, attributedText) = RACObserve(self, viewModel.year);
+        RAC(self.messageLabel, attributedText) = RACObserve(self, viewModel.message);
+        RAC(self.linkImageView, image) = RACObserve(self, viewModel.linkImage);
+        RAC(self.linkNameLabel, attributedText) = RACObserve(self, viewModel.linkName);
+        RAC(self.likesLabel, attributedText) = RACObserve(self, viewModel.likesSummary);
+        RAC(self.commentsLabel, attributedText) = RACObserve(self, viewModel.commentsSummary);
+
+        
     }
     return self;
 }

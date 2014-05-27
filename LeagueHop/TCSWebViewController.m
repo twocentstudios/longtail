95fd23d61281296f308351164b9c6e102b513893
Chris Trott <chris@twocentstudios.com>
2014-05-27 01:37:16 -0400

    Have to set loading manually

--- LeagueHop/TCSWebViewController.m
+++ LeagueHop/TCSWebViewController.m
@@ -25,20 +25,25 @@
 
     self.webView = [[UIWebView alloc] init];
     self.webView.delegate = self;
-    [self.view addSubview:self.webView];
+    [self.view insertSubview:self.webView atIndex:0];
 
     [self.webView rac_liftSelector:@selector(loadRequest:) withSignalsFromArray:@[RACObserve(self, viewModel.URLRequest)]];
-
-    RAC(self, loading) =
-        [RACSignal merge:@[
-            [[self.webView rac_signalForSelector:@selector(webViewDidStartLoad:) fromProtocol:@protocol(UIWebViewDelegate)] mapReplace:@YES],
-            [[self.webView rac_signalForSelector:@selector(webViewDidFinishLoad:) fromProtocol:@protocol(UIWebViewDelegate)] mapReplace:@NO] ]];
 
     [self rac_liftSelector:@selector(presentError:) withSignalsFromArray:@[[self.webView rac_signalForSelector:@selector(webView:didFailLoadWithError:) fromProtocol:@protocol(UIWebViewDelegate)]]];
 }
 
 - (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+
     self.webView.frame = self.view.bounds;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    self.loading = YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    self.loading = NO;
 }
 
 @end

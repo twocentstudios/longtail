9f33a7320e7ad6d4551a3ecd4257475c8b7c38ed
Chris Trott <chris@twocentstudios.com>
2014-06-03 22:58:31 -0400

    Fix long warning

--- LeagueHop/NSDate+TCSDateKey.m
+++ LeagueHop/NSDate+TCSDateKey.m
@@ -12,13 +12,13 @@
 
 - (NSString *)monthDayDateKey {
     NSDateComponents *components = [[NSCalendar currentCalendar] components:NSMonthCalendarUnit|NSDayCalendarUnit fromDate:self];
-    NSString *string = [NSString stringWithFormat:@"%02li%02li", components.month, components.day];
+    NSString *string = [NSString stringWithFormat:@"%02li%02li", (long)components.month, (long)components.day];
     return string;
 }
 
 - (NSString *)yearMonthDayDateKey {
     NSDateComponents *components = [[NSCalendar currentCalendar] components:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:self];
-    NSString *string = [NSString stringWithFormat:@"%04li%02li%02li", components.year, components.month, components.day];
+    NSString *string = [NSString stringWithFormat:@"%04li%02li%02li", (long)components.year, (long)components.month, (long)components.day];
     return string;
 }
 

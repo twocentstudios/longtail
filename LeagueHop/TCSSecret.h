8a79f1f29cbe52b34df3abc9d6b8d244dedfb0b8
Chris Trott <chris@twocentstudios.com>
2014-06-06 01:04:10 -0400

    Add TCSSecret

---
+++ LeagueHop/TCSSecret.h
@@ -0,0 +1,14 @@
+//
+//  TCSSecret.h
+//  LeagueHop
+//
+//  Created by Chris Trott on 6/6/14.
+//  Copyright (c) 2014 TwoCentStudios. All rights reserved.
+//
+
+#ifndef LeagueHop_TCSSecret_h
+#define LeagueHop_TCSSecret_h
+
+#define TCSTestFlightKey @""
+
+#endif
